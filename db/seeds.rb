a07255ebf6cd421a0fd20aaa44927d6f6d20dcfc
Alexander Tan <alexandertan527@gmail.com>
2025-03-23 21:28:27 +0000

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,32 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-require "json"
-require "open-uri"
 
-url = "https://tmdb.lewagon.com/movie/top_rated"
-movie_serialized = URI.parse(url).read
-movie = JSON.parse(movie_serialized)
+require 'uri'
+require 'net/http'
+require 'json'
 
-initial_path = 'https://image.tmdb.org/t/p/original'
+movies = []
 
-movie["results"].each do |movie|
-  Movie.create(title: movie["title"], overview: movie["overview"], poster_url: initial_path + movie["poster_path"], rating: movie["vote_average"])
+(1..13).each do |page|
+  url = URI("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=#{page}")
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+
+  request = Net::HTTP::Get.new(url)
+  request["accept"] = 'application/json'
+  request["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZmEyYTU5YTk1M2U3NjQ2ZTg1M2NkNWExNjlmZGNhNiIsIm5iZiI6MTc0Mjc1NjM0My4zMDU5OTk4LCJzdWIiOiI2N2UwNTlmN2MwYjQzODdhNmVjNzI5OGIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.LPwPCPtKCQhpB0T-9m_Pp8BxY9Q0z21LBgGLGoDZ7xc'
+
+  response = http.request(request)
+  data = JSON.parse(response.read_body)
+  movies.concat(data["results"])
+end
+
+movies = movies.first(250)
+
+movies.each do |movie|
+  base_url = 'https://image.tmdb.org/t/p/w300'
+  new_movie = Movie.new()
+  new_movie.save!
 end
